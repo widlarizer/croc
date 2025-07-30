ea92e8c07b30fa31af931b2e95c9280da20f6f3a
Emil J. Tywoniak <emil@tywoniak.eu>
2025-07-30 12:08:55 +0200

    demo: relative paths

--- yosys/scripts/expand-sdc.tcl
+++ yosys/scripts/expand-sdc.tcl
@@ -1,5 +1,5 @@
-read_verilog /home/emil/pulls/croc/yosys/tmp/croc_chip_yosys_elaborated_opensta.v
-read_lib /home/emil/pulls/croc/ihp13/pdk/ihp-sg13g2/libs.ref/sg13g2_io/lib/sg13g2_io_dummy.lib
+read_verilog $::env(TMP)/croc_chip_yosys_elaborated_opensta.v
+read_lib $::env(TMP)/../../ihp13/pdk/ihp-sg13g2/libs.ref/sg13g2_io/lib/sg13g2_io_dummy.lib
 link_design croc_chip
-read_sdc /home/emil/pulls/croc/openroad/src/constraints.sdc
-write_sdc /home/emil/pulls/croc/yosys/out/expanded_constraints.sdc
+read_sdc $::env(TMP)/../../openroad/src/constraints.sdc
+write_sdc $::env(TMP)/../../yosys/out/expanded_constraints.sdc
